62e756ef38394bcc77e57b9ab2a3587e83e3c807
John Michael Ferraris <johnmichaelferraris@gmail.com>
2017-11-30 16:21:59 +0800

    Initial commit for React-Redux

---
+++ src/client/app.jsx
@@ -0,0 +1,17 @@
+// @flow
+
+import React from 'react';
+import HelloButton from './containers/hello-button';
+import Message from './containers/message';
+import { APP_NAME } from '../shared/config';
+
+const App = () =>
+  (
+    <div>
+      <h1>{APP_NAME}</h1>
+      <Message />
+      <HelloButton />
+    </div>
+  );
+
+export default App;
---
+++ src/client/index.jsx
@@ -0,0 +1,45 @@
+// @flow
+
+import 'babel-polyfill';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+import App from './app';
+import helloReducer from './reducers/hello';
+import { APP_CONTAINER_SELECTOR } from '../shared/config';
+import { isProd } from '../shared/util';
+
+const NextApp = require('./app').default;
+
+/* eslint no-underscore-dangle: 0 */
+const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+/* eslint function-paren-newline: 0 */
+const store = createStore(combineReducers({ hello: helloReducer }),
+  composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+const rootEl = document.querySelector(APP_CONTAINER_SELECTOR);
+
+const wrapApp = (AppComponent, reduxStore) =>
+  (
+    <Provider store={reduxStore}>
+      <AppContainer>
+        <AppComponent />
+      </AppContainer>
+    </Provider>
+  );
+
+ReactDOM.render(wrapApp(App, store), rootEl);
+
+if (module.hot) {
+  // flow-disable-next-line
+  module.hot.accept('./app', () => {
+    // eslint-disable-next-line global-require
+
+    ReactDOM.render(wrapApp(NextApp, store), rootEl);
+  });
+}
